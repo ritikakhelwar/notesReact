0aa0458ffd17d317c8798b63c4b169b6fde1207f
RitikaKhelwar <sainiritika2659@gmail.com>
2023-07-30 23:57:40 +0530

    minior

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-    return (
-        <div className="container">
-            <h1>Home Page</h1>
-            <p>This is a notes app where you can create your notes and manage your notes.</p>
-        </div>
-    )
+  return (
+    <div className="container">
+      <h1>Home Page</h1>
+      <p>
+        This is a notes app where you can create your notes and manage your
+        notes. For mobile application will provide you apk link soon.😉
+      </p>
+      <img
+        src="https://d540vms5r2s2d.cloudfront.net/mad/uploads/mad_60c1fc35c19471623325749.png"
+        style={{ height: 380, width: 580 }}
+      />
+    </div>
+  );
 }
 
 export default Home;
